1e5329831c9c09bdba05094c3aaf527e2c6fca9e
max pc <huber.maxi@gmail.com>
2016-10-13 21:46:54 +0200

    added config

--- main.cc
+++ main.cc
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 //main program
 int main()
 {
- cout << "Hello World!" << endl; 
+ fstream cfgfile;
+ cfgfile.open("./testgen.cfg");
+ if(!cfgfile.is_open())
+ {
+    cfgfile.open("./testgen.cfg", ios::out);
+    cout << "config not found, generating config file testgen.cfg. Set your preferences." << endl;
+    cfgfile << "path/to/templates" << endl << "/path/for/temp/files" << endl;
+    cfgfile.close();
+    return 0;
+ }
+ string sTemplatesPath, sTempPath;
+ getline(cfgfile,sTemplatesPath);
+ getline(cfgfile,sTempPath);
+
+ cout << sTemplatesPath << endl;
  return 0;
 }
 
